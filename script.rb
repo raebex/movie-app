ff3b379b7f055dee2376c97ec6e62b0770a5c107
rebecca <r.young1025@gmail.com>
2021-01-19 19:48:25 -0600

    update script with new endpoints

--- script.rb
+++ script.rb
@@ -1,17 +1,17 @@
 require "http"
 
-response = HTTP.get("http://localhost:3000/api/single_actor_by_id")
+response = HTTP.get("http://localhost:3000/api/actors/10")
 single_actor_by_id = response.parse
 pp single_actor_by_id
 
-response = HTTP.get("http://localhost:3000/api/all_actors")
+response = HTTP.get("http://localhost:3000/api/actors")
 all_actors = response.parse
 pp all_actors
 
-response = HTTP.get("http://localhost:3000/api/single_movie/4")
+response = HTTP.get("http://localhost:3000/api/movies/4")
 single_movie = response.parse
 pp single_movie
 
-response = HTTP.get("http://localhost:3000/api/all_movies")
+response = HTTP.get("http://localhost:3000/api/movies")
 all_movies = response.parse
 pp all_movies
