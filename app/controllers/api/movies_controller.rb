92631110d16c70e8bb6c110730e76edabca6e1fb
rebecca <r.young1025@gmail.com>
2021-01-19 19:47:11 -0600

    make movies routes RESTful

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -9,4 +9,36 @@ class Api::MoviesController < ApplicationController
     @movie = Movie.find_by(id: params["id"])
     render "show.json.jb"
   end
+
+  def create
+    @movie = Movie.new(
+      {
+        title: params[:title],
+        year: params[:year],
+        plot: params[:plot]
+      }
+    )
+    @movie.save
+
+    render "show.json.jb"
+  end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.title = params[:title] || @movie.title
+    @movie.year = params[:year] || @movie.year
+    @movie.plot = params[:plot] || @movie.plot
+    @movie.save
+
+    render "show.json.jb"
+  end
+
+  def destroy
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    
+    render json: {
+      message: "Movie deleted!"
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
     get "/movies" => "movies#index"
     get "/movies/:id" => "movies#show"
-  
+    post "/movies" => "movies#create"
+    patch "/movies/:id" => "movies#update"
+    delete "/movies/:id" => "movies#destroy"
   end
 end
