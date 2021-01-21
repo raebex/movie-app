d5129b400e09edc96694c225121f0d78f39157dd
rebecca <r.young1025@gmail.com>
2021-01-20 19:48:36 -0600

    Add age and gender columns to the actor table

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -14,7 +14,9 @@ class Api::ActorsController < ApplicationController
       {
         first_name: params[:first_name],
         last_name: params[:last_name],
-        known_for: params[:known_for]
+        known_for: params[:known_for],
+        gender: params[:gender],
+        age: params[:age]
       }
     )
     @actor.save
@@ -27,6 +29,8 @@ class Api::ActorsController < ApplicationController
     @actor.first_name = params[:first_name] || @actor.first_name
     @actor.last_name = params[:last_name] || @actor.last_name
     @actor.known_for = params[:known_for] || @actor.known_for
+    @actor.gender = params[:gender] || @actor.gender
+    @actor.age = params[:age] || @actor.age
 
     @actor.save
 
