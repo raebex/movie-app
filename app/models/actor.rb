2c69bbd77485a011f75c0bee1cf3acfcc424bbbd
rebecca <r.young1025@gmail.com>
2021-01-25 16:15:25 -0600

    associate movies and actors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ApplicationRecord
+  belongs_to :movie
+  
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :known_for, presence: true
   validates :age, numericality: { greater_than: 13 }
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :actors
+  
   validates :title, :year, :director, presence: true
   validates :plot, length: { in: 10..500 }
 end
