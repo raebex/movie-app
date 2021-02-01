117a2f2ee89e3b7492b95ee35750684d7a24dfda
rebecca <r.young1025@gmail.com>
2021-02-01 09:44:07 -0600

    add many to many relationship between movies and genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,14 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   
   validates :title, :year, :director, presence: true
   validates :plot, length: { in: 10..500 }
+
+  def genre_names
+    genres.map do |genre|
+      genre.name
+    end
+  end
 end
